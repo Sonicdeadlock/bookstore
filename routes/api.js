32f0621d33346f3a56f6ac121a2625d9d59a2f01
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-04-06 00:33:48 -0400

    Added API Routing

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,14 @@
 var config = require('../config.js');
 var express = require('express');
 var router = express.Router();
+var books = require('./api/books');
+var professors = require('./api/professors');
+var courses = require('./api/courses');
+var cart = require('./api/cart');
 
+router.use('/books', books);
+router.use('/professors', professors);
+router.use('/courses', courses);
+router.use('/cart', cart);
 
 module.exports = router;
