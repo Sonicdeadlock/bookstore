48fcbd5817a8303785f20cb25968ec5ccbd76b3f
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-04-20 09:42:17 -0400

    Improved Login

--- public/components/navbar/loginController.js
+++ public/components/navbar/loginController.js
@@ -2,46 +2,19 @@ angular.module('controllers').controller('loginController', function ($scope, $h
     var errors = [];
     $scope.login = function () {
         $http.post('/auth/login', {username: $scope.username, password: $scope.password}).success(function (data) {
-                $rootScope.logged_in_user = {
-                    shippingInformation: {
-                        first_name: 'dummy name',
-                        last_name: 'dummy name',
-                        address: 'dummy address',
-                        state: 'dummy state',
-                        zip: 'dummy zip',
-                        city: 'dummy city'
-                    }
-                };
+                $rootScope.logged_in_user = data;
 
 
             })
             .error(function (data, status) {
-                errors.forEach(function (error) {
-                    error.$scope.$hide();
-                });
-                errors = [];
-                console.log(data);
                 if (data) {
-                    errors.push($alert({
-                        title: data.message,
-                        type: 'danger',
-                        show: true,
-                        container: '#alert-container'
-                    }));
+                    if (data == 'Invalid Username') {
+                        $scope.invalidUsername = true;
+                    }
+                    else if (data == 'Invalid Password') {
+                        $scope.invalidPassword = true;
+                    }
                 }
-                else if (status == 500)  errors.push($alert({
-                    title: "Internal server error",
-                    type: 'danger',
-                    show: true,
-                    container: '#alert-container'
-                }));
-                else   errors.push($alert({
-                        title: "Unknown error",
-                        type: 'danger',
-                        show: true,
-                        container: '#alert-container'
-                    }));
-                console.log(errors);
             })
     }
 });
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,22 @@ var router = express.Router();
 
 router.route('/login')
     .post(function (req, res) {
-        res.status(200).send();
+        if (req.body.password == '1234') {
+            res.status(200).json({
+                shippingInformation: {
+                    first_name: 'dummy name',
+                    last_name: 'dummy name',
+                    address: 'dummy address',
+                    state: 'dummy state',
+                    zip: 'dummy zip',
+                    city: 'dummy city'
+                }
+            });
+        }
+        else {
+            res.status(403).send('Invalid Password')
+        }
+
     });
 
 module.exports = router;
