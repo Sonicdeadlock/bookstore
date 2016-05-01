2833a3dcbc19dfa125383d715da81007f7aa55fd
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-05-01 14:23:12 -0400

    changed user login validation

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,11 @@ var router = express.Router();
 
 router.route('/login')
     .post(function (req, res) {
-        if (req.body.username.match(new RegExp('(^[A-Za-z]{5}[1-9]+)')) == null) {
+        if (req.body.username.match(new RegExp('(^[A-Za-z]{5,8}[1-9]{0,})')) == null) {
             res.status(403).send('Invalid Username');
         }
         else if (req.body.password == '12345678') {
-            res.status(200).json({
+            var user = {
                 shippingInformation: {
                     first_name: req.body.username[0],
                     last_name: req.body.username.substr(1, 4),
@@ -19,7 +19,9 @@ router.route('/login')
                     zip: '30043',
                     city: 'dummy city'
                 }
-            });
+            };
+            req.session.user = user;
+            res.status(200).json(user);
         }
         else {
             res.status(403).send('Invalid Password');
@@ -27,4 +29,12 @@ router.route('/login')
 
     });
 
+router.route('/self')
+    .get(function (req, res) {
+        if (req.session.user) {
+            res.json(req.session.user);
+        } else {
+            res.status(404).send('User not found')
+        }
+    });
 module.exports = router;
